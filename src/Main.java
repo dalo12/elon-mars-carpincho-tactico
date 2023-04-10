9a6f9d4dcb82756bd597d9f03e5fbc83a4852ec3
David López <dalo_vidpez@hotmail.com.ar>
2023-04-10 19:03:34 -0300

    Anda correctamente pero falta que la salida se muestre después de todas las entradas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
-import java.lang.Math.ceil;
+import java.util.List;
+import java.util.Scanner;
+import java.lang.Math;
 /*
  * Elon quiere viajar a Marte, pero necesita tu ayuda.
  * Elon tiene un cohete con una capacidad de W
@@ -7,14 +9,14 @@ import java.lang.Math.ceil;
  * Quiere llevar algunos de estos paquetes en el cohete, de forma tal que el cohete no vaya ni muy vacío ni muy cargado. En particular, el peso total C
  * de los paquetes debe satisfacer ⌈W/2⌉≤C≤W
  * Para ayudar a Elon, te pidió que imprimas una lista de paquetes que cumpla estas condiciones, o le avises que es imposible.
- * 
+ *
  * Made by David Latouquette & David López
  */
 
 
 /*
     Pseudocódigo:
- 
+
     para i = 0 hasta n{
         si w[i] <= W - C{
             solucion.add(i)
@@ -29,7 +31,7 @@ import java.lang.Math.ceil;
     }
  */
 
-public class Resolucion{
+public class Main{
     public static void main(String[] args){
         List<Integer> resultado;
         List<Integer> entrada = new ArrayList<Integer>();
@@ -38,6 +40,8 @@ public class Resolucion{
         int cant_n = 0;
         int peso_w = 0;
 
+        casos_t = s.nextInt();
+
         for(int i = 0; i<casos_t; i++){
             cant_n = s.nextInt();
             peso_w = s.nextInt();
@@ -45,33 +49,42 @@ public class Resolucion{
             for(int j = 0; j<cant_n; j++){
                 entrada.add(s.nextInt());
             }
-        }
 
         resultado = resolver(entrada, peso_w);
 
-        System.out.println();
-        for(Integer p : resultado){
-            System.out.print(p + " ");
+        if(resultado.isEmpty()){
+            System.out.println(-1);
+        }else{
+            System.out.println(resultado.size());
+            for(Integer p : resultado){
+                System.out.print(p + " ");
+            }
+            System.out.println();
         }
-        System.out.println();
+        entrada = new ArrayList<Integer>();
+        }
     }
 
-    protected List<Integer> resolver(List<Integer> entrada, int peso_w){
+    protected static List<Integer> resolver(List<Integer> entrada, int peso_w){
         List<Integer> resultado = new ArrayList<Integer>();
         int peso_c = 0;
-
+        int indice = 1;
         for(Integer p : entrada){
             if(p <= peso_w - peso_c){
-                resultado.add(entrada.indexOf(p));
+                resultado.add(indice);
                 peso_c += p;
             }
+            if(peso_w == peso_c){
+                break;
+            }
+
+            indice++;
         }
 
-        if(peso_c >= Math.ceil(peso_w / 2)){
+        if(peso_c < Math.ceil(peso_w / 2)){
             resultado.clear();
-            resultado.add(-1);
         }
 
-        return resultado;   
+        return resultado;
     }
 }
