0cb8ee1cad9789c32d252dfe64213b8f0719cd63
David López <dalo_vidpez@hotmail.com.ar>
2023-04-10 19:26:53 -0300

    Pasa test 1, pero falla en test 2

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.lang.Math;
-/*
- * Elon quiere viajar a Marte, pero necesita tu ayuda.
- * Elon tiene un cohete con una capacidad de W
- * Además, tiene n paquetes, donde el i-ésimo paquete pesa wi
- * Quiere llevar algunos de estos paquetes en el cohete, de forma tal que el cohete no vaya ni muy vacío ni muy cargado. En particular, el peso total C
- * de los paquetes debe satisfacer ⌈W/2⌉≤C≤W
- * Para ayudar a Elon, te pidió que imprimas una lista de paquetes que cumpla estas condiciones, o le avises que es imposible.
- *
- * Made by David Latouquette & David López
- */
-
-
-/*
-    Pseudocódigo:
-
-    para i = 0 hasta n{
-        si w[i] <= W - C{
-            solucion.add(i)
-            C = C + w[i]
-        }
-    }
-
-    si C >= W / 2 {
-        imprimir(solución)
-    }sino{
-        imprimir(-1)
-    }
- */
 
 public class Main{
     public static void main(String[] args) {
@@ -90,3 +62,32 @@ public class Main{
         return resultado;
     }
 }
+
+/*
+ * Elon quiere viajar a Marte, pero necesita tu ayuda.
+ * Elon tiene un cohete con una capacidad de W
+ * Además, tiene n paquetes, donde el i-ésimo paquete pesa wi
+ * Quiere llevar algunos de estos paquetes en el cohete, de forma tal que el cohete no vaya ni muy vacío ni muy cargado. En particular, el peso total C
+ * de los paquetes debe satisfacer ⌈W/2⌉≤C≤W
+ * Para ayudar a Elon, te pidió que imprimas una lista de paquetes que cumpla estas condiciones, o le avises que es imposible.
+ *
+ * Made by David Latouquette & David López
+ */
+
+
+/*
+    Pseudocódigo:
+
+    para i = 0 hasta n{
+        si w[i] <= W - C{
+            solucion.add(i)
+            C = C + w[i]
+        }
+    }
+
+    si C >= W / 2 {
+        imprimir(solución)
+    }sino{
+        imprimir(-1)
+    }
+ */
